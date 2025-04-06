8d6a5c8bfaada87c8f6b3634e6eef1fc938fd4ce
Pedro Jose Soto Rivera <pedroj.sotor@uqvirtual.edu.co>
2025-04-05 19:50:10 -0500

    Despacito

---
+++ src/main/java/co/edu/uniquindio/poo/bancointerfaz/Controller/Controller.java
@@ -0,0 +1,5 @@
+package co.edu.uniquindio.poo.bancointerfaz.Controller;
+
+public class Controller {
+
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,22 @@
+module co.edu.uniquindio.poo.bancointerfaz {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens co.edu.uniquindio.poo.bancointerfaz to javafx.fxml;
+    exports co.edu.uniquindio.poo.bancointerfaz;
+
+    opens co.edu.uniquindio.poo.bancointerfaz.Controller to javafx.fxml;
+    exports co.edu.uniquindio.poo.bancointerfaz.Controller;
+
+    opens co.edu.uniquindio.poo.bancointerfaz.ViewController to javafx.fxml;
+    exports co.edu.uniquindio.poo.bancointerfaz.ViewController;
+}
